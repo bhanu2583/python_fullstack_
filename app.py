0b564230c890edf6f0de5d802d8bc139e170cdbb
bhanu2583 <bhanupujari7569@gmail.com>
2026-09-01 05:47:42 +0000

    db connection

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app=Flask(__name__)
+
+app.secret_key="super_secret_key"
+
+def get_db_connection():
+    conn=sqlite3.connect("database.db")
+    conn.row_factory=sqlite3.Row
+    return conn
+
+def init_db():
+    conn=get_db_connection()
+    cursor=conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id  INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL,password TEXT NOT NULL,dob TEXT NOT NULL,gender TEXT NOT NULL,courses TEXT NOT NULL)")
+    conn.commit()
+    conn.close()
+init_db()
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
@@ -30,5 +47,17 @@ def login():
 @app.route("/trainers")
 def trainers():
     return render_template("trainers.html")
+@app.route("/api/register", methods=["POST"])
+def api_register():
+    data=request.get_json()
+    email=data.get("email")
+    conn=get_db_connection()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email=?", (email,))
+    existing_user=cursor.fetchone()
+    if existing_user:
+        conn.close()
+        return jsonify({"message": "Email already registered"})
+        name=data.get("password")
 if __name__=="__main__":
     app.run(debug=True)
